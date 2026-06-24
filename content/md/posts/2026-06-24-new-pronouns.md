d3930809cf231af796802a36b5adc40d0b55987a
Noah Bogart <noah.bogart@hey.com>
2026-06-24 14:00:49 -0400

    new post: new pronouns

---
+++ content/md/posts/2026-06-24-new-pronouns.md
@@ -0,0 +1,8 @@
+{:title "new pronouns"
+ :date "2026-06-16T21:26"
+ :at-uri "https://bsky.app/profile/noahbogart.com/post/3mobyib6oxs2w"
+ :tags ["gender"]}
+
+my 39th birthday is day after tomorrow. instead of a midlife crisis, i'm having Gender Feelings and will be changing my pronouns to [they/he](../../pronouns) (they/them preference, he/him in a pinch).
+
+thanks in advance.
--- themes/lotus/js/bluesky.js
+++ themes/lotus/js/bluesky.js
@@ -1,11 +1,125 @@
+// taken from https://github.com/gohjy/bsky-to-aturi
+
+async function resolveHandle(handle) {
+    const fetchUrl = new URL("https://public.api.bsky.app/xrpc/com.atproto.identity.resolveHandle");
+    fetchUrl.searchParams.set("handle", handle);
+    const fetchResult = await fetch(fetchUrl.href).catch(() => ({
+        error: "FetchError",
+        message: "Failed to fetch Bluesky API"
+    }));
+    if (fetchResult.error) return fetchResult;
+
+    const json = await fetchResult.json().catch(() => ({
+        error: "ServerError",
+        message: "Bluesky API did not return valid JSON"
+    }));
+
+    if (json.did) return json.did;
+    return json;
+}
+
+const REGEX = {
+    // https://atproto.com/specs/did#at-protocol-did-identifier-syntax
+    DID: /^(did:[a-z]+:[a-zA-Z0-9._:%-]*)\/app.bsky.feed.post\/([a-zA-Z0-9._-]*)$/,
+
+    // https://atproto.com/specs/handle#handle-identifier-syntax
+    HANDLE: /^([a-zA-Z0-9]([a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]([a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?$/,
+
+    // https://github.com/bluesky-social/atproto/blob/main/lexicons/app/bsky/feed/post.json#L8 specifies app.bsky.feed.post records must have TID rkey
+    // https://atproto.com/specs/tid#tid-syntax
+    TID: /^[234567abcdefghij][234567abcdefghijklmnopqrstuvwxyz]{12}$/
+};
+
+function destructureBskyUrl(bskyUrl) {
+    // "at://did:plc:qbidoe2gpkfelnurqod37ikr/app.bsky.feed.post/3mlu74wihik27"
+    const urlObj = (() => {
+        try {
+            return new URL(bskyUrl);
+        } catch {
+            if (bskyUrl.startsWith("at://"))
+                return {pathname: bskyUrl.replace("at://", "")}
+
+            return null;
+        }
+    })();
+    if (!urlObj) return null;
+
+    const pathname = urlObj.pathname;
+    console.log(pathname);
+
+    const [_didMatch, repo, rkey] = pathname.match(REGEX.DID) || [];
+    if (_didMatch) {
+        return {
+            type: "did",
+            repo,
+            rkey
+        };
+    }
+
+    const [_fullMatch, handleOrDid, rkey2] = pathname.match(/^\/profile\/([^\/]+)(?:\/(?:post\/([^\/]+)\/?)?)?$/) || [null];
+
+    if (!_fullMatch) return null;
+    if (rkey && !rkey.match(REGEX.TID)) return null;
+
+    if (handleOrDid.match(REGEX.HANDLE)) {
+        return {
+            type: "handle", 
+            repo: handleOrDid,
+            rkey: rkey2,
+        };
+    } else if (handleOrDid.match(REGEX.DID)) {
+        return {
+            type: "did",
+            repo: handleOrDid,
+            rkey: rkey2,
+        };
+    }
+
+    return null;
+}
+
+function constructUri({repo, rkey}) {
+    if (!rkey) {
+        // default to app.bsky.actor.profile
+        return `at://${repo}/app.bsky.actor.profile/self`;
+    }
+    return `at://${repo}/app.bsky.feed.post/${rkey}`;
+}
+
+async function getUri(bskyUrl) {
+    const urlComponents = destructureBskyUrl(bskyUrl);
+    if (!urlComponents) throw {
+        error: "InvalidUrl",
+        message: "Invalid Bluesky URL provided"
+    };
+
+    if (urlComponents.type === "did") {
+        // just construct URI and return
+        return constructUri(urlComponents);
+    } else {
+        // it's a handle
+        // 1. resolve handle
+        const did = await resolveHandle(urlComponents.repo);
+        if (did.error) throw did;
+
+        // 2. construct URI and return
+        return constructUri({
+            repo: did,
+            rkey: urlComponents.rkey
+        });
+    }
+}
+
 // taken from https://capscollective.com/blog/bluesky-blog-comments/
 
 function getPostLinkFromUri(uri) {
     return `https://bsky.app/profile/${uri.replace("at://", "").replace("/app.bsky.feed.post/", "/post/")}`;
 }
+
 function getImageLinkFromBlob(did, blobLink, useThumbnail) {
     return `https://cdn.bsky.app/img/${useThumbnail ? "feed_thumbnail" : "feed_fullsize"}/plain/${did}/${blobLink}`;
 }
+
 function createHtmlTextForRecord(record) {
     var htmlText = `<span>`
 
@@ -49,6 +163,7 @@ function createHtmlTextForRecord(record) {
 
     return htmlText + '</span>';
 }
+
 function createHtmlForReplies(replies) {
     var htmlText = ""
     for (const replyIdx in replies) {
@@ -96,21 +211,26 @@ function createHtmlForReplies(replies) {
 
 function httpGetAsJsonAsync(url, callback) {
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() { 
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-            var responseJson = JSON.parse(xmlHttp.responseText);
-            if (typeof(responseJson) == 'undefined') {
-                return;
+    getUri(url).then(uri => {
+        const bskyUrl = "https://public.api.bsky.app/xrpc/app.bsky.feed.getPostThread?uri=" + uri;
+        console.log(bskyUrl);
+        xmlHttp.onreadystatechange = function() { 
+            if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+                var responseJson = JSON.parse(xmlHttp.responseText);
+                if (typeof(responseJson) == 'undefined') {
+                    return;
+                }
+                var element = document.getElementById("show-comments");
+                element.parentNode.removeChild(element);
+                callback(responseJson);
             }
-            var element = document.getElementById("show-comments");
-            element.parentNode.removeChild(element);
-            callback(responseJson);
         }
-    }
-    xmlHttp.open("GET", url, true);
-    xmlHttp.send(null);
+        xmlHttp.open("GET", bskyUrl, true);
+        xmlHttp.send(null);
+    });
 }
-function responseCallback (responseJson) {
+
+function responseCallback(responseJson) {
     const thread = responseJson["thread"];
     const threadPost = thread["post"];
     const postLink = getPostLinkFromUri(threadPost.uri);
